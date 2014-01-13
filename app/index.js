3d947158b111a954ef7e0a89c18a88d52ea7bfe2
David Pustai <david@pustai.cz>
2014-01-13 01:31:03 +0100

    Create basic structure with working Gruntfile

--- app/index.js
+++ app/index.js
@@ -1,3 +1,21 @@
+/*
+ * TODO:
+ *  - humans.txt
+ *  - Gruntfile.js - serve:open and livereload
+ *  - main.scss
+ *  - features
+ *  - sublime path
+ *  - override normalize settings
+ *  - import normalize
+ *  - foundation sass modules
+ *  - gitignore
+ *  - editorconfig
+ *  - rev, htmlmin
+ *  - ask for appname
+ *  - GA
+ *
+ */
+
 'use strict';
 var util = require('util');
 var path = require('path');
@@ -7,10 +25,6 @@ var yeoman = require('yeoman-generator');
 var WebProjectGenerator = module.exports = function WebProjectGenerator(args, options, config) {
   yeoman.generators.Base.apply(this, arguments);
 
-  this.on('end', function () {
-    this.installDependencies({ skipInstall: options['skip-install'] });
-  });
-
   this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
 };
 
@@ -19,32 +33,92 @@ util.inherits(WebProjectGenerator, yeoman.generators.Base);
 WebProjectGenerator.prototype.askFor = function askFor() {
   var cb = this.async();
 
-  // have Yeoman greet the user.
+  // welcome message
   console.log(this.yeoman);
+  console.log('Foundation5 with Compass and HTML5Boilerplate are prepared!');
 
   var prompts = [{
-    type: 'confirm',
-    name: 'someOption',
-    message: 'Would you like to enable this option?',
-    default: true
+    type: 'checkbox',
+    name: 'features',
+    message: 'What more would you like?',
+    choices: [{
+      name: 'Magnific Popup',
+      value: 'includeMagnificPopup',
+      checked: false
+    }, {
+      name: 'Fontello Fonts',
+      value: 'includeFontello',
+      checked: false
+    }]
   }];
 
-  this.prompt(prompts, function (props) {
-    this.someOption = props.someOption;
+  this.prompt(prompts, function (answers) {
+    var features = answers.features;
+
+    function hasFeature(feat) { return features.indexOf(feat) !== -1; }
+
+    // manually deal with the response, get back and store the results.
+    // we change a bit this way of doing to automatically do this in the self.prompt() method.
+    this.includeMagnificPopup = hasFeature('includeMagnificPopup');
+    this.includeFontello = hasFeature('includeFontello');
 
     cb();
   }.bind(this));
 };
 
-WebProjectGenerator.prototype.app = function app() {
-  this.mkdir('app');
-  this.mkdir('app/templates');
-
-  this.copy('_package.json', 'package.json');
-  this.copy('_bower.json', 'bower.json');
+WebProjectGenerator.prototype.gruntfile = function gruntfile() {
+  this.template('Gruntfile.js');
 };
 
-WebProjectGenerator.prototype.projectfiles = function projectfiles() {
-  this.copy('editorconfig', '.editorconfig');
-  this.copy('jshintrc', '.jshintrc');
+WebProjectGenerator.prototype.packageJSON = function packageJSON() {
+  this.template('_package.json', 'package.json');
+};
+
+WebProjectGenerator.prototype.git = function git() {
+  this.copy('gitignore', '.gitignore');
+  this.copy('gitattributes', '.gitattributes');
+};
+
+WebProjectGenerator.prototype.bower = function bower() {
+  this.template('_bower.json', 'bower.json');
+};
+
+/*WebProjectGenerator.prototype.sublime = function sublime() {
+  this.template('project.sublime-project', this.name+'.sublime-project');
+};*/
+
+WebProjectGenerator.prototype.fs = function fs() {
+  this.mkdir('css');
+  this.mkdir('font');
+  this.mkdir('img');
+  this.mkdir('js');
+  this.mkdir('sass');
+};
+
+WebProjectGenerator.prototype.files = function files() {
+  this.copy('404.html', '404.html');
+
+  this.copy('favicon.ico', 'favicon.ico');
+  this.copy('apple-touch-icon-precomposed.png', 'apple-touch-icon-precomposed.png');
+
+  this.copy('htaccess', '.htaccess');
+  this.copy('robots.txt', 'robots.txt');
+  this.copy('crossdomain.xml', 'crossdomain.xml')
+
+  this.copy('h5bp.scss','sass/_h5bp.scss');
+  this.copy('main.scss','sass/main.scss');
+
+  this.copy('main.js', 'js/main.js');
+};
+
+WebProjectGenerator.prototype.index = function index() {
+
+  this.copy('index.html', 'index.html');
+};
+
+WebProjectGenerator.prototype.install = function () {
+  var done = this.async();
+  this.installDependencies({
+    callback: done
+  });
 };
---
+++ app/templates/Gruntfile.js
@@ -0,0 +1,255 @@
+// Generated on <%= (new Date).toISOString().split('T')[0] %> using <%= pkg.name %> <%= pkg.version %>
+'use strict';
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// use this if you want to recursively match all subfolders:
+// 'test/spec/**/*.js'
+
+module.exports = function (grunt) {
+
+    // Project configuration
+    grunt.initConfig({
+
+        // Watches files for changes and runs tasks based on the changed files
+        watch: {
+            compass: {
+                files: ['sass/*.scss'],
+                tasks: ['compass', 'autoprefixer'],
+                options: {
+                    spawn: false,
+                    interrupt: true
+                }
+            },
+            livereload: {
+                options: {
+                    livereload: true
+                },
+                files: [
+                    '*.html',
+                    'css/*.css',
+                    'img/*.{gif,jpeg,jpg,png,svg}',
+                    'js/*.js'
+                ]
+            }
+        },
+
+        // The actual grunt server settings
+        connect: {
+            options: {
+                port: 8000,
+                livereload: 35729,
+                hostname: 'localhost'
+            },
+            dev: {
+                options: {
+                    base: '.',
+                    //open: true
+                }
+            },
+            dist: {
+                options: {
+                    //open: true,
+                    base: 'dist',
+                    livereload: false
+                }
+            }
+        },
+
+        // Empties folders to start fresh
+        clean: {
+            dist: {
+                files: [{
+                    dot: true,
+                    src: [
+                        '.tmp',
+                        'dist/*',
+                        '!dist/.git*'
+                    ]
+                }]
+            },
+            server: '.tmp'
+        },
+
+        // Compiles Sass to CSS and generates necessary files if requested
+        compass: {
+            dist: {
+                options: {
+                    sassDir: 'sass',
+                    cssDir: '.tmp',
+                    noLineComments: true
+                }
+            }
+        },
+
+        // Add vendor prefixed styles
+        autoprefixer: {
+            options: {
+                browsers: ['last 2 version', 'ie >= 6']
+            },
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '.tmp',
+                    src: '*.css',
+                    dest: 'css'
+                }]
+            }
+        },
+
+        // Automatically inject Bower components into the HTML file
+        // 'bower-install': {
+        //     app: {
+        //         html: '/index.html',
+        //         ignorePath: '/'
+        //     }
+        // },
+
+        // Renames files for browser caching purposes
+        // rev: {
+        //     dist: {
+        //         files: {
+        //             src: [
+        //                 '/scripts/{,*/}*.js',
+        //                 '/styles/{,*/}*.css',
+        //                 '/images/{,*/}*.{gif,jpeg,jpg,png,webp}',
+        //                 '/styles/fonts/{,*/}*.*'
+        //             ]
+        //         }
+        //     }
+        // },
+
+        // Reads HTML for usemin blocks to enable smart builds that automatically
+        // concat, minify and revision files. Creates configurations in memory so
+        // additional tasks can operate on them
+        useminPrepare: {
+            options: {
+                dest: 'dist'
+            },
+            html: 'index.html'
+        },
+
+        // Performs rewrites based on rev and the useminPrepare configuration
+         usemin: {
+             /*options: {
+                 assetsDirs: ['dist']
+             },*/
+             html: ['dist/*.html']/*,
+             css: ['dist/css/*.css']*/
+         },
+
+        // The following *-min tasks produce minified files in the dist folder
+        imagemin: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'img',
+                    src: '*.{gif,jpeg,jpg,png}',
+                    dest: 'dist/img'
+                }]
+            }
+        },
+        svgmin: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'img',
+                    src: '*.svg',
+                    dest: 'dist/img'
+                }]
+            }
+        },
+        // htmlmin: {
+        //     dist: {
+        //         options: {
+        //             collapseBooleanAttributes: true,
+        //             collapseWhitespace: true,
+        //             removeAttributeQuotes: true,
+        //             removeCommentsFromCDATA: true,
+        //             removeEmptyAttributes: true,
+        //             removeOptionalTags: true,
+        //             removeRedundantAttributes: true,
+        //             useShortDoctype: true
+        //         },
+        //         files: [{
+        //             expand: true,
+        //             cwd: 'dist',
+        //             src: '{,*/}*.html',
+        //             dest: 'dist'
+        //         }]
+        //     }
+        // },
+
+        // Copies remaining files to places other tasks can use
+        copy: {
+            dist: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: '.',
+                    dest: 'dist',
+                    src: [
+                        '*.{ico,png,txt,xml}',
+                        '.htaccess',
+                        '*.html',
+                        'font/*.{svg,ttf,eot,woff}'
+                    ]
+                }]
+            }
+        },
+
+        // Generates a custom Modernizr build that includes only the tests you
+        // reference in your app
+        // modernizr: {
+        //     devFile: '/bower_components/modernizr/modernizr.js',
+        //     outputFile: '/bower_components/modernizr/modernizr.js',
+        //     files: [
+        //         '/scripts/{,*/}*.js',
+        //         '/styles/{,*/}*.css',
+        //         '!/scripts/vendor/*'
+        //     ],
+        //     uglify: true
+        // },
+
+        // Run some tasks in parallel to speed up build process
+        concurrent: {
+            dist: [
+                'compass',
+                'imagemin',
+                'svgmin'
+            ]
+        }
+    });
+
+    // Load grunt tasks automaticly
+    require('load-grunt-tasks')(grunt);
+
+    grunt.registerTask('default', [
+        'clean:server',
+        'compass',
+        'autoprefixer',
+        'connect:test',
+        'watch'
+    ]);
+
+    grunt.registerTask('serve', [
+        'build',
+        'connect:dist:keepalive'
+    ]);
+
+    grunt.registerTask('build', [
+        'clean:dist',
+        'useminPrepare',
+        'concurrent:dist',
+        'autoprefixer',
+        'concat',
+        'cssmin',
+        'uglify',
+        'copy:dist',
+        //'modernizr',
+        //'rev',
+        'usemin',
+        //'htmlmin'
+    ]);
+};
