b6c32cb05769bee5e7044032c45b6772ed6d2958
David Pustai <david@pustai.cz>
2014-03-11 16:18:03 +0100

    Move assets to separate directory

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -9,257 +9,236 @@
 
 module.exports = function (grunt) {
 
-    // Project configuration
-    grunt.initConfig({
+	// Project configuration
+	grunt.initConfig({
 
-        // Watches files for changes and runs tasks based on the changed files
-        watch: {
-            compass: {
-                files: ['sass/*.scss'],
-                tasks: ['compass', 'autoprefixer'],
-                options: {
-                    spawn: false,
-                    interrupt: true
-                }
-            },
-            livereload: {
-                options: {
-                    livereload: true
-                },
-                files: [
-                    '*.html',
-                    'sass/*.scss',
-                    //'css/*.css',
-                    'img/*.{gif,jpeg,jpg,png,svg}',
-                    'js/*.js'
-                ]
-            }
-        },
+		// Watches files for changes and runs tasks based on the changed files
+		watch: {
+			compass: {
+				files: ['assets/sass/*.scss'],
+				tasks: ['compass', 'autoprefixer'],
+				options: {
+					spawn: false,
+					interrupt: true
+				}
+			},
+			livereload: {
+				options: {
+					livereload: true
+				},
+				files: [
+					'*.html',
+					'assets/sass/*.scss',
+					//'assets/css/*.css',
+					'assets/img/*.{gif,jpeg,jpg,png,svg}',
+					'assets/js/*.js'
+				]
+			}
+		},
 
-        // The actual grunt server settings
-        connect: {
-            options: {
-                port: 8000,
-                livereload: 35729,
-                hostname: '0.0.0.0'
-            },
-            dev: {
-                options: {
-                    base: '.',
-                    //open: true
-                }
-            },
-            dist: {
-                options: {
-                    //open: true,
-                    base: 'dist',
-                    livereload: false
-                }
-            }
-        },
+		// The actual grunt server settings
+		connect: {
+			options: {
+				port: 8000,
+				livereload: 35729,
+				hostname: '0.0.0.0'
+			},
+			dev: {
+				options: {
+					base: '.',
+					//open: true
+				}
+			},
+			dist: {
+				options: {
+					//open: true,
+					base: 'dist',
+					livereload: false
+				}
+			}
+		},
 
-        // Empties folders to start fresh
-        clean: {
-            dist: {
-                files: [{
-                    dot: true,
-                    src: [
-                        '.tmp',
-                        'dist/*',
-                        '!dist/.git*'
-                    ]
-                }]
-            },
-            server: '.tmp'
-        },
+		// Empties folders to start fresh
+		clean: {
+			dist: {
+				files: [{
+					dot: true,
+					src: [
+						'.tmp',
+						'dist/*',
+						'!dist/.git*'
+					]
+				}]
+			},
+			server: '.tmp'
+		},
 
-        // Compiles Sass to CSS and generates necessary files if requested
-        compass: {
-            dist: {
-                options: {
-                    sassDir: 'sass',
-                    cssDir: '.tmp',
-                    noLineComments: true
-                }
-            }
-        },
+		// Compiles Sass to CSS and generates necessary files if requested
+		compass: {
+			dist: {
+				options: {
+					sassDir: 'assets/sass',
+					cssDir: '.tmp',
+					noLineComments: true
+				}
+			}
+		},
 
-        // Add vendor prefixed styles
-        autoprefixer: {
-            options: {
-                browsers: ['last 2 version', 'ie >= <%= IEsupport %>']
-            },
-            dist: {
-                files: [{
-                    expand: true,
-                    cwd: '.tmp',
-                    src: '*.css',
-                    dest: 'css'
-                }]
-            }
-        },
+		// Add vendor prefixed styles
+		autoprefixer: {
+			options: {
+				browsers: ['last 2 version', 'ie >= <%= IEsupport %>']
+			},
+			dist: {
+				files: [{
+					expand: true,
+					cwd: '.tmp',
+					src: '*.css',
+					dest: 'assets/css'
+				}]
+			}
+		},
 
-        uglify: {
-            options: {
-                mangle: {toplevel: true},
-                squeeze: {dead_code: false},
-                codegen: {quote_keys: true}
-            }
-        },
+		/*uncss: {
+			dist: {
+				options: {
+					//stylesheets: ['assets/css/main.css']
+				},
+				files: {
+					'css/tidy.css': ['*.html']
+				}
+			}
+		},*/
 
-        // Automatically inject Bower components into the HTML file
-        // 'bower-install': {
-        //     app: {
-        //         html: '/index.html',
-        //         ignorePath: '/'
-        //     }
-        // },
+		uglify: {
+			options: {
+				//mangle: {toplevel: true},
+				squeeze: {dead_code: false},
+				codegen: {quote_keys: true}
+			},
+			jquery: {
+				src: 'bower_components/jquery/jquery.min.js',
+				dest: 'dist/assets/js/jquery.js'
+			}
+		},
 
-        // Renames files for browser caching purposes
-        // rev: {
-        //     dist: {
-        //         files: {
-        //             src: [
-        //                 '/scripts/{,*/}*.js',
-        //                 '/styles/{,*/}*.css',
-        //                 '/images/{,*/}*.{gif,jpeg,jpg,png,webp}',
-        //                 '/styles/fonts/{,*/}*.*'
-        //             ]
-        //         }
-        //     }
-        // },
+		// Reads HTML for usemin blocks to enable smart builds that automatically
+		// concat, minify and revision files. Creates configurations in memory so
+		// additional tasks can operate on them
+		useminPrepare: {
+			options: {
+				dest: 'dist'
+			},
+			html: 'index.html'
+		},
 
-        // Reads HTML for usemin blocks to enable smart builds that automatically
-        // concat, minify and revision files. Creates configurations in memory so
-        // additional tasks can operate on them
-        useminPrepare: {
-            options: {
-                dest: 'dist'
-            },
-            html: 'index.html'
-        },
+		// Performs rewrites based on rev and the useminPrepare configuration
+		 usemin: {
+			 /*options: {
+				 assetsDirs: ['assets']
+			 },*/
+			 html: ['dist/*.html']/*,
+			 css: ['dist/assets/css/*.css']*/
+		 },
 
-        // Performs rewrites based on rev and the useminPrepare configuration
-         usemin: {
-             /*options: {
-                 assetsDirs: ['dist']
-             },*/
-             html: ['dist/*.html']/*,
-             css: ['dist/css/*.css']*/
-         },
+		// The following *-min tasks produce minified files in the dist folder
+		imagemin: {
+			dist: {
+				files: [{
+					expand: true,
+					cwd: 'assets/img',
+					src: '*.{gif,jpeg,jpg,png}',
+					dest: 'dist/assets/img'
+				}]
+			}
+		},
+		svgmin: {
+			dist: {
+				files: [{
+					expand: true,
+					cwd: 'assets/img',
+					src: '**/*.svg',
+					dest: 'dist/assets/img'
+				}]
+			}
+		},
+		htmlmin: {
+			dist: {
+				options: {
+					removeComments: true,
+					removeCommentsFromCDATA: true,
+					collapseWhitespace: true,
+					collapseBooleanAttributes: true,
+					removeAttributeQuotes: true,
+					removeRedundantAttributes: true,
+					useShortDoctype: true,
+					removeEmptyAttributes: true,
+					removeOptionalTags: true
+				},
+				files: [{
+					expand: true,
+					cwd: 'dist',
+					src: '*.html',
+					dest: 'dist'
+				}]
+			}
+		},
 
-        // The following *-min tasks produce minified files in the dist folder
-        imagemin: {
-            dist: {
-                files: [{
-                    expand: true,
-                    cwd: 'img',
-                    src: '*.{gif,jpeg,jpg,png}',
-                    dest: 'dist/img'
-                }]
-            }
-        },
-        svgmin: {
-            dist: {
-                files: [{
-                    expand: true,
-                    cwd: 'img',
-                    src: '*.svg',
-                    dest: 'dist/img'
-                }]
-            }
-        },
-        htmlmin: {
-            dist: {
-                options: {
-                	removeComments: true,
-                	removeCommentsFromCDATA: true,
-                	collapseWhitespace: true,
-                    collapseBooleanAttributes: true,
-                    removeAttributeQuotes: true,
-                    removeRedundantAttributes: true,
-                    useShortDoctype: true,
-                    removeEmptyAttributes: true,
-                    removeOptionalTags: true
-                },
-                files: [{
-                    expand: true,
-                    cwd: 'dist',
-                    src: '*.html',
-                    dest: 'dist'
-                }]
-            }
-        },
+		// Copies remaining files to places other tasks can use
+		copy: {
+			dist: {
+				files: [{
+					expand: true,
+					dot: true,
+					cwd: '.',
+					dest: 'dist',
+					src: [
+						'*.{ico,png,txt,xml}',
+						'.htaccess',
+						'*.html',
+						'assets/font/*.{svg,ttf,eot,woff}'
+					]
+				}]
+			}
+		},
 
-        // Copies remaining files to places other tasks can use
-        copy: {
-            dist: {
-                files: [{
-                    expand: true,
-                    dot: true,
-                    cwd: '.',
-                    dest: 'dist',
-                    src: [
-                        '*.{ico,png,txt,xml}',
-                        '.htaccess',
-                        '*.html',
-                        'font/*.{svg,ttf,eot,woff}'
-                    ]
-                }]
-            }
-        },
+		// Run some tasks in parallel to speed up build process
+		concurrent: {
+			dist: [
+				'compass',
+				'imagemin',
+				'svgmin'
+			]
+		}
+	});
 
-        // Generates a custom Modernizr build that includes only the tests you
-        // reference in your app
-        // modernizr: {
-        //     devFile: '/bower_components/modernizr/modernizr.js',
-        //     outputFile: '/bower_components/modernizr/modernizr.js',
-        //     files: [
-        //         '/scripts/{,*/}*.js',
-        //         '/styles/{,*/}*.css',
-        //         '!/scripts/vendor/*'
-        //     ],
-        //     uglify: true
-        // },
+	// Load grunt tasks automaticly
+	require('load-grunt-tasks')(grunt);
 
-        // Run some tasks in parallel to speed up build process
-        concurrent: {
-            dist: [
-                'compass',
-                'imagemin',
-                'svgmin'
-            ]
-        }
-    });
+	grunt.registerTask('default', [
+		'clean:server',
+		'compass',
+		'autoprefixer',
+		'connect:dev',
+		'watch'
+	]);
 
-    // Load grunt tasks automaticly
-    require('load-grunt-tasks')(grunt);
+	grunt.registerTask('serve', [
+		'build',
+		'connect:dist:keepalive'
+	]);
 
-    grunt.registerTask('default', [
-        'clean:server',
-        'compass',
-        'autoprefixer',
-        'connect:dev',
-        'watch'
-    ]);
-
-    grunt.registerTask('serve', [
-        'build',
-        'connect:dist:keepalive'
-    ]);
-
-    grunt.registerTask('build', [
-        'clean:dist',
-        'useminPrepare',
-        'concurrent:dist',
-        'autoprefixer',
-        'concat',
-        'cssmin',
-        'uglify',
-        'copy:dist',
-        //'modernizr',
-        //'rev',
-        'usemin',
-        'htmlmin'
-    ]);
+	grunt.registerTask('build', [
+		'clean:dist',
+		'useminPrepare',
+		'concurrent:dist',
+		'autoprefixer',
+		//uncss,
+		'concat',
+		'cssmin',
+		'uglify',
+		'copy:dist',
+		'usemin',
+		'htmlmin'
+	]);
 };
--- app/templates/config.rb
+++ app/templates/config.rb
@@ -3,8 +3,8 @@ add_import_path "bower_components/foundation/scss"
 add_import_path "bower_components"
 
 # Sass Directories
-javascripts_dir = 'js'
-css_dir = 'css'
-sass_dir = 'sass'
-images_dir = 'img'
-fonts_dir = 'font'
+javascripts_dir = 'assets/js'
+css_dir = 'assets/css'
+sass_dir = 'assets/sass'
+images_dir = 'assets/img'
+fonts_dir = 'assets/font'
